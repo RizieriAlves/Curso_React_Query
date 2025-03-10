126cac34c9ec34253e4220fffefb0cebde35a1d1
RizieriAlves <viniciusrizieri@hotmail.com>
2025-03-10 20:15:26 -0300

    sec. 4 e 5. Centralization, custom hooks, pre-fetching e pagination

---
+++ Lazy Days/client/src/components/staff/hooks/useStaff.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+import type { Staff } from "@shared/types";
+
+import { filterByTreatment } from "../utils";
+
+import { axiosInstance } from "@/axiosInstance";
+import { queryKeys } from "@/react-query/constants";
+
+// query function for useQuery
+async function getStaff(): Promise<Staff[]> {
+  const { data } = await axiosInstance.get("/staff");
+  return data;
+}
+
+export function useStaff() {
+  // for filtering staff by treatment
+  const [filter, setFilter] = useState("all");
+
+  const fallback: Staff[] = [];
+  // TODO: get data from server via useQuery
+  const { data = fallback } = useQuery({
+    queryKey: [queryKeys.staff],
+    queryFn: getStaff,
+  });
+  const staff: Staff[] = data;
+
+  return { staff, filter, setFilter };
+}
